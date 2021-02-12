61f891062ebdf33a236cb9a2f6d6ae64b0354ca2
hattoemi <a7b8i06c49@outlook.com>
2021-02-12 16:47:00 +0800

    via

--- src/MidoiEngine.jl
+++ src/MidoiEngine.jl
@@ -1,5 +1,15 @@
 module MidoiEngine
 
-# Write your package code here.
+# using,import,include & export
+
+const MOVIE_FPS = 24
+const COMMON_FPS = 30
+const HIGHER_FPS = 60
+const SUPER_HIGHER_FPS = 120
+const HIGHEST_FPS = 144
+
+using AbstractPlotting, Makie, CairoMakie
+
+export editor, reader
 
 end
